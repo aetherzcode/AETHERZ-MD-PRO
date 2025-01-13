1abe59e30e3af8740bd4cb7c0a7e1fcf746b18ff
AETHER <aetherzcode@gmail.com>
2025-01-13 16:53:00 +0700

    Update file plugins/info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -1,29 +1,16 @@
-import fs from "fs";
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn }) => {
   let totalf = Object.values(global.plugins).filter((v) => v.help && v.tags).length;
-  let text = `AETHERZ-MD is online, Sir.`;
+  let text = `AETHERZ-MD memiliki Total fitur: ${totalf}`;
 
-  // Unduh gambar thumbnail
   let response = await fetch("https://files.catbox.moe/3cj9sd.jpg");
   let thumbnailBuffer = await response.buffer();
 
-  // Baca file audio dari lokasi media
-  let audioBuffer = fs.readFileSync("media/alvyna0.mp3");
-
-  conn.sendFile(
+  conn.sendMessage(
     m.chat,
-    audioBuffer,
-    "yuhumina.mp3",
-    text,
-    m,
-    true,
     {
-      type: "audioMessage",
-      ptt: true,
-      mimetype: 'audio/mp4',
-      fileName: "yuhumina.mp3",
+      text: text,
       contextInfo: {
         externalAdReply: {
           showAdAttribution: true,
@@ -31,7 +18,7 @@ let handler = async (m, { conn }) => {
           mediaUrl: "url_audio",
           title: "T O T A L - F I T U R",
           body: `📁 ${totalf} aktif`,
-          sourceUrl: link.web,
+          sourceUrl: "https://aetherz.xyz",
           thumbnail: thumbnailBuffer
         }
       }
