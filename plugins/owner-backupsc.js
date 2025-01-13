039fb3c91f28042bba739c8cc4f13b5b34ae8e0e
AETHER <aetherzcode@gmail.com>
2025-01-13 22:34:19 +0700

    Update file plugins/owner-backupsc.js

--- plugins/owner-backupsc.js
+++ plugins/owner-backupsc.js
@@ -8,38 +8,30 @@ const handler = async (m, { conn, isROwner }) => {
    try {
       let zipFileName = `AETHERZ-MD-PRO.zip`;
 
-      setTimeout(() => {
-         if (fs.existsSync("node_modules")) {
-            m.reply("Folder 'node_modules' tidak ikut di backup.");
-         }
-         
-         const file = fs.readFileSync('./AETHERZ-MD-PRO.zip');
-         conn.sendMessage(
-            m.chat,
-            {
-               document: file,
-               mimetype: "application/zip",
-               fileName: zipFileName,
-               caption: "Backup selesai. Silakan unduh file backup.",
-            },
-            { quoted: m }
-         );
+      if (fs.existsSync("node_modules")) {
+         m.reply("Folder 'node_modules' tidak ikut di backup.");
+      }
 
-         setTimeout(() => {
-            fs.unlinkSync(zipFileName);
-            m.reply("File backup telah dihapus.");
-         }, 5000);
-      }, 3000);
+      let zipCommand = `zip -r ${zipFileName} * -x "node_modules/*"`;
+      await exec_(zipCommand);
+
+      const file = fs.readFileSync(zipFileName);
+      conn.sendMessage(
+         m.chat,
+         {
+            document: file,
+            mimetype: "application/zip",
+            fileName: zipFileName,
+            caption: "Backup selesai. Silakan unduh file backup.",
+         },
+         { quoted: m }
+      );
 
       setTimeout(() => {
-         let zipCommand = `zip -r ${zipFileName} * -x "node_modules/*"`;
-         exec_(zipCommand, (err, stdout, stderr) => {
-            if (err) {
-               console.error(`Error: ${stderr}`);
-               m.reply("Terjadi kesalahan saat membuat file zip.");
-            }
-         });
-      }, 1000);
+         fs.unlinkSync(zipFileName);
+         m.reply("File backup telah dihapus.");
+      }, 5000);
+
    } catch (error) {
       m.reply("Terjadi kesalahan saat melakukan backup.");
       console.error(error);
