dee5991319ee44ef4ceb9e9ba963deda443d501d
AETHER <aetherzcode@gmail.com>
2025-01-16 02:27:24 +0700

    Update file plugins/ai-remini.js

--- plugins/ai-remini.js
+++ plugins/ai-remini.js
@@ -1,76 +1,30 @@
-import axios from 'axios'
-import FormData from 'form-data'
-import { fileTypeFromBuffer } from 'file-type'
-let handler = async (m, {
-   conn,
-   usedPrefix,
-   command,
-   args
-}) => {
-   try {
-      let q = m.quoted ? m.quoted : m
-      let mime = (q.msg || q).mimetype || ''
-      if (!mime) return conn.reply(m.chat, 'Reply photo.', m)
-      if (!/image\/(jpe?g|png)/.test(mime)) return conn.reply(m.chat, 'Only for photo.', m)
-      m.reply('Wait...')
-      let img = await q.download()
-      let image = await uploader(img)
-      let json = await (await axios.get('https://api.alyachan.dev/api/remini?image=' + image.data.url + '&apikey=Ariel1')).data
-      if (!json.status) return conn.reply(m.chat, String(json), m)
-      conn.sendFile(m.chat, json.data.url, '', '', m)
-   } catch (e) {
-      return conn.reply(m.chat, String(e), m)
-   }
-}
-handler.help = ['remini', 'color', 'hdr']
-handler.tags = ['ai']
-handler.limit = true
-handler.command = ['remini', 'color', 'hdr', 'hd']
-export default handler
+import fetch from 'node-fetch';
+import uploadImage from '../lib/uploadImage.js';
 
-async function uploader(buffer) {
-   return new Promise(async (resolve) => {
-      try {
-         const { ext } = await fileTypeFromBuffer(buffer)
-         const form = new FormData()
-         form.append('file', buffer, 'tmp.' + ext)
-         const json = await (await axios.post("https://tmpfiles.org/api/v1/upload", form, {
-            headers: {
-               "accept": "/",
-               "accept-language": "id-ID , id q=O. 9 , en- US  q=0.8, en q=0.7",
-               "content-type": "multipart/form-data",
-               "origin": "https://tmpfiles.orgi",
-               "referer": "https://tmpfiles.org/",
-               "sec-ch-ua": '"Chromium"v="107", "Not=A?Brand"v="24"',
-               "sec-ch-ua-mobile": "?1",
-               "sec-ch-ua-platform": "Android",
-               "sec-fetch-dest": "empty",
-               "sec-fetch-mcde": "cors",
-               "sec-fetch-site": "same-origin",
-               "user-agent": "Mozilla/5.0 (Linux Android 6.0.1 SM-J500G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36",
-               "x-requested-with": "XMLHttpRequest",
-               ...form.getHeaders()
-            }
-         })).data
-         if (json.status != 'success') return resolve({
-            developer: '@naando.io',
-            status: false,
-            msg: 'Failed to uploaded'
-         })
-         resolve({
-            developer: '@naando.io',
-            status: true,
-            data: {
-               url: json.data.url.replace('https://tmpfiles.org/', 'https://tmpfiles.org/dl/')
-            }
-         })
-      } catch (e) {
-         console.log(e)
-         resolve({
-            creator: global.creator,
-            status: false,
-            msg: e.message
-         })
-      }
-   })
+async function handler(m, { conn, usedPrefix, command }) {
+  try {
+    const q = m.quoted ? m.quoted : m;
+    const mime = (q.msg || q).mimetype || q.mediaType || '';
+    if (/^image/.test(mime) && !/webp/.test(mime)) {
+      const img = await q.download();
+      const out = await uploadImage(img);
+      const api = await fetch(`https://api.betabotz.eu.org/api/tools/remini?url=${out}&apikey=${lann}`);
+      const image = await api.json();
+      const { url } = image 
+       conn.sendFile(m.chat, url, null, wm, m);
+    } else {
+      m.reply(`Kirim gambar dengan caption *${usedPrefix + command}* atau tag gambar yang sudah dikirim.`);
+    }
+  } catch (e) {
+    console.error(e);
+    m.reply(`Identifikasi gagal. Silakan coba lagi.`);
+  }
 }
+
+handler.help = ['remini'];
+handler.tags = ['tools'];
+handler.command = ['remini'];
+handler.premium = false;
+handler.limit = true;
+
+export default handler;
