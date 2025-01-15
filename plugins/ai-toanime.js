df948d2d50d335becedcce1fd1a39f920823c89e
AETHER <aetherzcode@gmail.com>
2025-01-16 02:38:03 +0700

    Update file plugins/ai-toanime.js

--- plugins/ai-toanime.js
+++ plugins/ai-toanime.js
@@ -1,67 +1,34 @@
-import axios from 'axios';
-import uploadImage from '../lib/uploadImage.js'
+import uploadImage from '../lib/uploadImage';
+import fetch from 'node-fetch';
+let handler = async (m, { 
+conn, 
+usedPrefix, 
+command
+ }) => {
+	var q = m.quoted ? m.quoted : m
+	var mime = (q.msg || q).mimetype || q.mediaType || ''
+	if (/image/g.test(mime) && !/webp/g.test(mime)) {
+		await conn.reply(m.chat, wait, m)
+		try {
+			const img = await q.download?.()
+			let out = await uploadImage(img)
+			let old = new Date()
+			let res = await fetch(`https://api.betabotz.eu.org/api/maker/jadianime?url=${out}&apikey=${lann}`)
+			let convert = await res.json()
+			let buff = await fetch(convert.result.img_crop_single)
+  .then(res => res.buffer())
+			await conn.sendMessage(m.chat, { image: buff, caption: `🍟 *Fetching* : ${((new Date - old) * 1)} ms` }, { quoted: m })
+		} catch (e) {
+			console.log(e)
+			m.reply(`[ ! ] Identifikasi Gagal.`)
+		}
+	} else {
+		m.reply(`Kirim gambar dengan caption *${usedPrefix + command}* atau tag gambar yang sudah dikirim`)
+	}
+};
+handler.help = ['jadianime'];
+handler.command = ['toanime', 'jadianime'];
+handler.tags = ['maker'];
+handler.limit = 5;
 
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-    conn.toanime = conn.toanime ? conn.toanime : {};
-    if (m.chat in conn.toanime) {
-        return m.reply("_Mohon Tunggu Sebentar, Masih Ada Proses Yang Belum Selesai_");
-    }
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (!mime) throw 'Send Foto Dengan Caption .hdr'
-    m.reply("Tunggu sebentar, sedang memproses...")
-    let media = await q.download()
-    let url = await uploadImage(media)
-    conn["toanime"][m.chat] = true;
-    try {
-        const openAIResponse = await processImageAndUpload(media);
-
-        if (openAIResponse) {
-            const result = openAIResponse;
-            const tag = `@${m.sender.split('@')[0]}`;
-
-            await conn.sendMessage(m.chat, {
-                image: {
-                    url: result
-                },
-                caption: `_Succes To Anime Style_`,
-                mentions: [m.sender]
-            }, {
-                quoted: m
-            })
-        } else {
-            console.log("Tidak ada respons dari OpenAI atau terjadi kesalahan.");
-        }
-    } catch (e) {
-        conn.sendFile(m.chat, 'Terjadi kesalahan saat memproses gambar.', "error.mp3", null, m, true, {
-            type: "audioMessage",
-            ptt: true,
-        })
-    }
-    delete conn.toanime[m.chat];
-}
-handler.command = /^(toanime|jadianime)$/i
-handler.help = ['toanime']
-handler.tags = ['ai']
-handler.register = false
-handler.limit = true
-
-export default handler
-
-async function processImageAndUpload(buffer) {
-    try {
-        const base64String = Buffer.from(buffer, 'binary').toString('base64');
-
-        const apiResponse = await axios.post('https://www.drawever.com/api/photo-to-anime', {
-            data: `data:image/png;base64,${base64String}`,
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        return 'https://www.drawever.com' + apiResponse.data.urls[1] || 'https://www.drawever.com' + apiResponse.data.urls[0];
-    } catch (error) {
-        throw error;
-    }
-}
+export default handler;;
