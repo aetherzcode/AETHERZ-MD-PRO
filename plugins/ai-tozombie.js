89b8f67cb7551170dbf298a5f2a672aedfc771b6
AETHER <aetherzcode@gmail.com>
2025-01-16 02:39:43 +0700

    Update file plugins/ai-tozombie.js

--- plugins/ai-tozombie.js
+++ plugins/ai-tozombie.js
@@ -1,27 +1,34 @@
+import uploadImage from '../lib/uploadImage';
 import fetch from 'node-fetch';
-import uploadImage from '../lib/uploadImage.js'
+let handler = async (m, { 
+conn, 
+usedPrefix, 
+command
+ }) => {
+	var q = m.quoted ? m.quoted : m
+	var mime = (q.msg || q).mimetype || q.mediaType || ''
+	if (/image/g.test(mime) && !/webp/g.test(mime)) {
+    await conn.reply(m.chat, wait, m)
+		try {
+			const img = await q.download?.()
+			let out = await uploadImage(img)
+			let old = new Date()
+			let res = await fetch(`https://api.betabotz.eu.org/api/maker/jadizombie?url=${out}&apikey=${lann}`)
+			let convert = await res.json()
+			let buff = await fetch(convert.result)
+  .then(res => res.buffer())
+			await conn.sendMessage(m.chat, { image: buff, caption: `🍟 *Fetching* : ${((new Date - old) * 1)} ms` }, { quoted: m })
+		} catch (e) {
+			console.log(e)
+			m.reply(`[ ! ] Identifikasi Gagal.`)
+		}
+	} else {
+		m.reply(`Kirim gambar dengan caption *${usedPrefix + command}* atau tag gambar yang sudah dikirim`)
+	}
+};
+handler.help = ['jadizombie'];
+handler.command = ['jadizombie'];
+handler.tags = ['ai'];
+handler.limit = 5;
 
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-conn.tozombie = conn.tozombie ? conn.tozombie : {};
-if (m.chat in conn.tozombie) {
-return m.reply("_Mohon Tunggu Sebentar, Masih Ada Proses Yang Belum Selesai_");
-}
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
-if (!mime) throw 'Send Foto Dengan Caption .hdr'
-m.reply(wait)
-let media = await q.download()
-let url = await uploadImage(media)
-conn["tozombie"][m.chat] = true;
-let anu = await fetch(`https://aemt.me/converter/zombie?url=${url}`)
-let hasil = await anu.json()
-await conn.sendFile(m.chat, hasil.url, 'anu.jpg', '_Succes To Zombie Style_', m)
-delete conn.tozombie[m.chat];
-}
-handler.command = /^(tozombie|jadizombie)$/i
-handler.help = ['tozombie']
-handler.tags = ['aiv2']
-handler.register = false
-handler.limit = true
-
-export default handler
+export default handler;;
