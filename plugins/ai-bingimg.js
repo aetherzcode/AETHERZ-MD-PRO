29bcbad08f7e96db9e9fa691db3d87b4ebeaf030
AETHER <aetherzcode@gmail.com>
2025-01-17 18:02:08 +0700

    Update file plugins/ai-bingimg.js

--- plugins/ai-bingimg.js
+++ plugins/ai-bingimg.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 if (!text) throw 'Masukan Query'
 m.reply(wait)
-let sange = await fetch (`https://aemt.me/bingimg?text=${text}`)
+let sange = await fetch (`https://api.betabotz.eu.org/api/search/bing-img?text=${text}&apikey=${global.lann}`)
 let anu = await sange.json()
 conn.sendFile(m.chat, anu.result, 'sange.jpg', '_Nih Kak Hasilnya_', m)
 }
