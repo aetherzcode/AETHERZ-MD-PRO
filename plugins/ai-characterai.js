77874f5af4578deb70811bba53b22c39fbf02909
AETHER <aetherzcode@gmail.com>
2025-01-17 17:57:24 +0700

    Update file plugins/ai-characterai.js

--- plugins/ai-characterai.js
+++ plugins/ai-characterai.js
@@ -1,19 +1,33 @@
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn, text, command }) => {
-  if (command == 'rimuru') {
-    if (!text) return conn.reply(m.chat, `.${command} hallo rimuru`, m);
+  if (command == 'kirito') {
+    if (!text) return conn.reply(m.chat, `.${command} hallo kirito`, m);
     conn.sendMessage(m.chat, { react: { text: '🕒', key: m.key } });
-    apikey: "Salsa"
-    let res = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?text=${text}&name=rimuru`);
-    let kemii = await res.json();
-    conn.reply(m.chat, `${kemii.result}`, m);
+
+    try {
+      let res = await fetch(`https://api.betabotz.eu.org/api/search/c-ai?prompt=${text}&char=Kirito&apikey=${global.lann}`);
+      
+      if (!res.ok) {
+        throw new Error('API request failed');
+      }
+
+      let eterr = await res.json();
+      
+      if (eterr.message) {
+        conn.reply(m.chat, `${eterr.message}`, m);
+      } else {
+        conn.reply(m.chat, 'Tidak ada respons yang valid dari API.', m);
+      }
+    } catch (error) {
+      console.error(error);
+      conn.reply(m.chat, 'Terjadi kesalahan saat memproses permintaan ke API.', m);
+    }
   }
 };
 
-handler.help = ['rimuru', 'kemii', 'jokowi', 'megawati', 'yaemiko', 'paimon', 'kiku', 'putin', 'lisa'].map(v => v + ' *<teks>*');
-handler.command = /^rimuru|kemii|jokowi|megawati|yaemiko|paimon|kiku|putin|lisa$/i;
+handler.help = ['rimuru', 'kirito', 'eterr', 'jokowi', 'megawati', 'yaemiko', 'paimon', 'kiku', 'putin', 'lisa'].map(v => v + ' *<teks>*');
+handler.command = /^rimuru|kirito|eterr|jokowi|megawati|yaemiko|paimon|kiku|putin|lisa$/i;
 handler.tags = ['ai'];
-handler.limit = true;
 
 export default handler;
