1d4f38d017a211d7aea73393f6f4a4b29ff519ee
AETHER <aetherzcode@gmail.com>
2025-01-31 12:54:38 +0700

    Update downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -10,7 +10,7 @@ const handler = async (m, { usedPrefix, command, conn, args }) => {
   try {
     const query = encodeURIComponent(args.join(" "));
     const response = await fetch(
-      `https://api.ryzendesu.vip/api/search/pinterest?query=${query}`
+      `${APIs.ryzem}/api/search/pinterest?query=${query}`
     );
     const data = await response.json();
 
